cab2cc480a7adc8a3239e4d510447b0de9f9c564
Satyam Sarkar <satyam.sarkar1404@gmail.com>
2022-01-17 16:34:57 +0530

    Update

--- buildSrc/src/main/java/BuildManager.kt
+++ buildSrc/src/main/java/BuildManager.kt
@@ -3,7 +3,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 open class SubInfo{
-    var submoduleNames: String? = null
+    var workingDirectory: String = ""
+    var submoduleList: List<String> = emptyList()
 }
 
 class BuildManager : Plugin<Project> {
@@ -12,9 +13,15 @@ class BuildManager : Plugin<Project> {
 
         val extension = project.extensions.create("submodules",SubInfo::class.java)
 
+        project.exec{
+            workingDir(extension.workingDirectory)
+            extension.submoduleList.forEach { urlPath ->
+                commandLine("git","submodule","add", urlPath )
+            }
+        }
+
         project.task("printSomething") {
             doLast {
-                println("Extension submodules are ${extension.submoduleNames}")
                 println("Welcome from gradle world!")
             }
         }
